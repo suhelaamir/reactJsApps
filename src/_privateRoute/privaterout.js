679d83158df3077e4d8fc83ddc9c07578ae57e8f
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-07 09:34:47 +0530

    Edited

--- src/_privateRoute/privaterout.js
+++ src/_privateRoute/privaterout.js
@@ -3,7 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 import App from '../_components/app';
 
 export const PrivateRout = ({component: Component, ...rest}) => {
-    //
     return <Route {...rest} render={props => {
         return localStorage.getItem('userDetails') ? <App><Component {...props} /></App> : <Redirect to={{pathname : 'auth/login', state: {from: props.location}}}/>
     }} />  
