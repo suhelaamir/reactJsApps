20b4268da13754487405f742245f343754ca2b62
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-09 05:40:34 +0530

    Added menu file atuh header file enums for the operations and the path for API

---
+++ src/_helpers/dbOperation.js
@@ -0,0 +1,8 @@
+const DBOperation = Object.freeze({
+    create: 1,
+    update: 2,
+    _delete: 3,
+    view: 4
+});
+
+export default DBOperation;
---
+++ src/_helpers/global.js
@@ -0,0 +1,8 @@
+export default class Global {
+    //for live server
+    static BASE_API_PATH = "";
+
+    //for Image path
+    static BASE_IMAGES_PATH = "";
+    static BASE_USER_IMAGES_PATH = "";
+}
