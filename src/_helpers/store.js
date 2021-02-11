fb2f98e54293aab2aff598c402cc36a08eb4139b
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-11 07:03:19 +0530

    Added store - using thunk for assynchronus

---
+++ src/_helpers/store.js
@@ -0,0 +1,20 @@
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../_reducers/user.reducer';
+
+//you can create multiple store
+const mainReducer = combineReducers({
+    reducer
+});
+
+//https://github.com/zalmoxisus/redux-devtools-extension
+
+//passing the main store to the createStore function
+//use thunk to implement assynchronus call to API
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(mainReducer, composeEnhancers(
+        applyMiddleware(thunk)
+    ));
+
+export default store;
