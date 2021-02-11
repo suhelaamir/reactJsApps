4e8cb8d78f5bbd0875a953fb2a73b48023e9b7b9
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-11 06:25:37 +0530

    Added FromValidator - to validate the input

---
+++ src/_validators/formValidator.js
@@ -0,0 +1,43 @@
+import validator from 'validator';
+
+class FromValidator {
+    constructor(validations) {
+        this.validations = validations;
+    }
+
+    validate(state, col) {
+        let validation = this.valid();
+        this.validations.forEach(rule => {
+
+            if (!validation[rule.field].isInvalid) {
+                let field_value = null;
+                if (col === "") {
+                    field_value = state[rule.field].toString();
+                } else {
+                    field_value = state[col][rule.field].toString();
+                }
+
+                const args = rule.args || [];
+                const validation_method = typeof rule.method === 'string' ? validator[rule.method] : rule.method;
+
+                if (validation_method(field_value, ...args, state) !== rule.validWhen) {
+                    validation[rule.field] = { isInvalid: true, message: rule.message };
+                    validation.isValid = false;
+                }
+            }
+        });
+        return validation;
+    }
+
+    valid() {
+        const validation = {};
+
+        this.validations.map(rule => (
+            validation[rule.field] = { isInvalid: false, message: '' }
+        ));
+
+        return { isValid: true, ...validation }
+    }
+}
+
+export default FromValidator;
