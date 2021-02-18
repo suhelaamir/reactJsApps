ce3781a318816ad9cba060217839db06f6a3806b
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-18 18:08:04 +0530

    Edited - calling API using fetch

--- src/_services/login/user.service.js
+++ src/_services/login/user.service.js
@@ -1,5 +1,35 @@
+import Global from '../../_helpers/global';
 
 export const userService={
-    login,
-    register
+    login
+}
+
+async function login(username, password) {
+    debugger;
+    const requestOptions = {
+        method: 'POST',
+        header: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username, password})
+    }
+
+    return fetch(Global.BASE_API_PATH + 'UserMaster/Login/', requestOptions)
+        .then(handleResponse)
+    .then(res => {
+        return res;
+    });
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+
+        if(!response.ok) {
+            if(response.status === 401) {
+
+            }
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        return data;
+    });
 }
