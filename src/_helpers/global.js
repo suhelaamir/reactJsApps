883403c800171bc101dc0c8e3c6422e229308b6c
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-18 18:12:01 +0530

    Edited - Declare the global variable here.

--- src/_helpers/global.js
+++ src/_helpers/global.js
@@ -1,8 +1,16 @@
 export default class Global {
-    //for live server
-    static BASE_API_PATH = "";
+    // //for live server
+    // static BASE_API_PATH = "";
 
-    //for Image path
-    static BASE_IMAGES_PATH = "";
-    static BASE_USER_IMAGES_PATH = "";
+    // //for Image path
+    // static BASE_IMAGES_PATH = "";
+    // static BASE_USER_IMAGES_PATH = "";
+
+    // For live serve
+    static BASE_API_PATH = "https://localhost:44350/api/";
+
+
+    // For Image Path
+    static BASE_IMAGES_PATH = "https://localhost:44350/images/";
+    static BASE_USER_IMAGES_PATH = "https://localhost:44350/users/";
 }
